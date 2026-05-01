2f66d98d316587f65543f71ceb91226b70fdaffa
new version <any>
2026-05-01 12:59:06 +0530

    feat: add logs and audit logs workflows with revert and reason support

---
+++ env.js
@@ -0,0 +1,16 @@
+﻿  window.__ENV = {
+  firebase: {
+    apiKey: "AIzaSyAgcgkP_pDfN4LzWQhYA9lWQCcwQkDyXS0",
+    authDomain: "phoenix-ims.firebaseapp.com",
+    projectId: "phoenix-ims",
+    storageBucket: "phoenix-ims.firebasestorage.app",
+    messagingSenderId: "188386831338",
+    appId: "1:188386831338:web:49db0bcf5c873236d7e093",
+    measurementId: "G-TZ0GVZWTN1"
+  },
+  stockAlertEmail: {
+    apiUrl: "https://princepenguinbiotech.github.io/Phoenix-IMS/api/stock-alert",
+    apiKey: "phoenix-ims-alert-2026",
+    toEmail: "phoenixpathlabs@gmail.com"
+  }
+};
